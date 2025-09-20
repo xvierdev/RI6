1866a09b7b1182ffb9f402c028e37ebc1677b759
Wesley Xavier <71525241+xvierdev@users.noreply.github.com>
2025-09-19 21:36:42 -0300

    Delete out directory
    Removidos arquivos não utilizados

--- out/calculo.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Calculo = /** @class */ (function () {
-    function Calculo() {
-    }
-    return Calculo;
-}());
-exports.default = Calculo;
